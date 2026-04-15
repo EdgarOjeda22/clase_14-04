d152160acdd7376b21032aed6ec1b5b574a1c14a
EdgarOjeda22 <edgar.ojeda@inacapmail.cl>
2026-04-14 20:47:50 -0400

    nuevos cambios

---
+++ clase_14-04/form.js
@@ -0,0 +1,43 @@
+document.getElementById("formulario").addEventListener("submit", function (e) {
+    e.preventDefault();
+    
+    let inputs = document.querySelectorAll("#formulario input", "#formulario textarea");
+    let edad = document.querySelector("input[type= ´number´]").value;
+    let email = document.querySelector("input[type= ´email´]").value;
+    let valido = true;
+    let mensaje = "";
+
+    //validar edad
+    inputs.forEach(function (input) {
+        if (input.value.trim() === "") {
+            valido = false;
+            mensaje = "Todos los campos deben estar completos";
+        }
+
+    })
+
+
+    if (edad < 18) {
+        valido = false;
+        mensaje = "Debes tener al menos 18 años";
+        console.log(">>>" + edad)
+
+    }
+
+
+    if (!email.incluides("@") || !email.incluides(".")) {
+        valido = false;
+        mensaje = "correo inválido";
+
+
+    }
+
+    if (valido) {
+        window.location.href = "valido.html"
+    }
+    else {
+        alert(mensaje);
+    }
+
+
+});
